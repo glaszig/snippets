c869ee37e8788994ac839acf6aa648562544f690
glaszig <glaszig@gmail.com>
2010-04-29 11:38:36 +0200

    adding jquery plugins

---
+++ jquery-plugins/jquery.image-updatable.js
@@ -0,0 +1,71 @@
+(function($) {
+
+	$.fn.updatable = function(config) {
+	
+		config = $.extend({
+			debug: false,
+			//period: 3000,
+			queryKey: '_jq-update',
+			queryValue: function() {
+				return new Date().getTime();
+			},
+			loadingClass: 'jq-updatable-loading',
+			onBeforeLoad: function() {
+				$(this).addClass(config.loadingClass);
+			},
+			onAfterLoad: function() {
+				$(this).removeClass(config.loadingClass);
+			}
+		}, config);
+		
+		var log = function() {
+			if(config.debug && console && console.log) {
+				console.log.apply(console.log, arguments);
+			}
+		}
+		
+		var periodicalUpdater = function(imgElement, period) {
+			var myself = arguments.callee;
+			setTimeout(function() {
+				config.onBeforeLoad.call(imgElement);
+				// todo: update image src url
+				var oldSrc = imgElement.attr('src').split('?');
+				var qs = oldSrc[1] || ''; //config.queryKey+'=now';
+				// clean up
+				qs = qs.replace(/&*$/, '').replace(
+					new RegExp('&?'+config.queryKey+'=[^&]*&?', 'ig'), ''
+				)+'&';
+				// set src to initiate reload
+				imgElement.attr('src', oldSrc[0]+'?'+qs+config.queryKey+'='+config.queryValue());
+				log(imgElement.attr('src'));
+				// repeating...
+				myself.call(myself, imgElement, period);
+			}, period);
+		};
+		
+		return this.each(function() {
+			
+			var img = $(this);
+			
+			img.load(function() {
+				config.onAfterLoad.call($(this));
+			});
+			
+			// update periodical
+			if(config.period) {
+				periodicalUpdater(img, config.period);
+			}
+			
+		});
+	};
+
+})(jQuery);
+/**
+ * usage example
+ */
+/*
+$('img').updatable({
+	debug: true,
+	period:3000
+});
+*/
