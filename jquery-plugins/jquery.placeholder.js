5de6b99d285d67173f56a4f349b0cc3dac8c143b
glaszig <glaszig@gmail.com>
2010-12-15 18:14:04 +0100

    adding anonymous namespace and options to jquery placeholder plugin

--- jquery-plugins/jquery.placeholder.js
+++ jquery-plugins/jquery.placeholder.js
@@ -5,36 +5,43 @@
  * @link github.com/glaszig/snippets
  * @license see link
  */
-$.fn.placeholder = function(options) {
-	
-	return this.filter('[type=text]').each(function() {
-		
-		var changed = false;
-		
-		var setPlaceholder = function(input) {
-			if(input.value == '' || !changed) {
-				input.value = $(input).addClass('jq-has-placeholder').attr('placeholder');
-				changed = false;
-			}
-		}
-		var clearPlaceholder = function(input) {
-			if(!changed && input.value == $(input).attr('placeholder')) {
-				$(input).removeClass('jq-has-placeholder').val('');
-			}
-		}
-		
-		setPlaceholder(this);
-		
-		$(this).change(function() {
-			changed = true;
-			setPlaceholder(this);
-		}).click(function() {
-			clearPlaceholder(this);
-		}).blur(function() {
-			setPlaceholder(this);
-		});
-		
-	});
-	
-}
+(function($) {
 
+	$.fn.placeholder = function(options) {
+	
+		options = $.extend({
+			toggleClass : 'jq-has-placeholder'
+		}, options);
+	
+		return this.filter('[type=text]').each(function() {
+		
+			var changed = false;
+		
+			var setPlaceholder = function(input) {
+				if(input.value == '' || !changed) {
+					input.value = $(input).addClass('jq-has-placeholder').attr('placeholder');
+					changed = false;
+				}
+			}
+			var clearPlaceholder = function(input) {
+				if(!changed && input.value == $(input).attr('placeholder')) {
+					$(input).removeClass('jq-has-placeholder').val('');
+				}
+			}
+		
+			setPlaceholder(this);
+		
+			$(this).change(function() {
+				changed = true;
+				setPlaceholder(this);
+			}).click(function() {
+				clearPlaceholder(this);
+			}).blur(function() {
+				setPlaceholder(this);
+			});
+		
+		});
+	
+	}
+	
+})(jQuery);
