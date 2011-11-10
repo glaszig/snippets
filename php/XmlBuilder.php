2f37f977f5b0277b84e73431f934100e0ee65452
glaszig <glaszig@gmail.com>
2011-11-10 16:08:07 +0100

    adding xml builder class

---
+++ php/XmlBuilder.php
@@ -0,0 +1,114 @@
+<?php
+
+/**
+ * XML builder class
+ * Inspired by Rails' XMLBuilder.
+ *
+ * Usage:
+ * {{{
+ *  $xml = new XmlBuilder;
+ *  $xml->foo->bar->baz;
+ *  $xml->foo->bla = 'blubb';
+ *  $xml->foo->bla['title'] = 'empty';
+ *  echo $xml;
+ * }}}
+ *
+ * @author glaszig at gmail dot com
+ */
+class XmlBuilderElement implements ArrayAccess {
+    
+    protected $element = null;
+    protected $attributes = array();
+    protected $children = array();
+    
+    public function __construct($name) {
+        $this->element = new DOMElement($name);
+    }
+    
+    public function __get($name) {
+        return $this->child($name);
+    }
+    
+    public function __set($name, $value) {
+        return $this->child($name)->value($value);
+    }
+    
+    public function child($name) {
+        if (!isset($this->children[$name]))
+            $this->children[$name] = new XmlBuilderElement($name);
+        return $this->children[$name];
+    }
+    
+    protected function value() {
+        if (func_num_args() == 1) {
+            $this->element->nodeValue = func_get_arg(0);
+            return $this;
+        }
+        return $this->element->nodeValue;
+    }
+    
+    public function offsetExists($offset) {
+        return isset($this->attributes[$offset]);
+    }
+    
+    public function offsetGet($offset) {
+        return $this->attributes[$offset];
+    }
+        
+    public function offsetSet($offset, $value) {
+        $this->attributes[$offset] = $value;
+    }
+        
+    public function offsetUnset($offset) {
+        unset($this->attributes[$offset]);
+    }
+    
+    public function &element() {
+        return $this->element;
+    }
+    
+    public function toXml($version = '1.0', $encoding = 'UTF-8') {
+        $dom = new DOMDocument($version, $encoding);
+        $rootNode = $dom->appendChild($this->element);
+        $this->build($rootNode);
+        return $dom->saveXML();
+    }
+    
+    protected function build($dom = null) {
+        foreach ($this->children as $child) {
+            $node = $dom->appendChild($child->element());
+            $child->build($node);
+        }
+        foreach ($this->attributes as $name => $value) {
+            $this->element->setAttribute($name, $value);
+        }
+        return $dom;
+    }
+    
+    public function __toString() {
+        return $this->toXml();
+    }
+    
+}
+
+class XmlBuilder extends XmlBuilderElement {
+    
+    public function __construct($version = '1.0', $encoding = 'UTF-8') {
+        $this->element = new DOMDocument($version, $encoding);
+        $this->init();
+    }
+    
+    protected function init() {
+        $this->element->formatOutput = true;
+    }
+    
+    public function instruct() {
+        echo '<?xml version="'.$this->element->xmlVersion.'" encoding="'.$this->element->xmlEncoding.'" ?>';
+    }
+  
+    public function toXml() {
+        return $this->build($this->element)->saveXML();
+    }
+}
+
+?>
