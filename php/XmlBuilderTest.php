4d795a8c271b739780ee44fa10af2e27e488b18b
Peter Schultz <peter.schultz@classmarkets.com>
2012-04-23 14:35:54 +0200

    adding more unit tests

--- php/XmlBuilderTest.php
+++ php/XmlBuilderTest.php
@@ -4,14 +4,59 @@ require_once 'simpletest/autorun.php';
 require_once 'XmlBuilder.php';
 
 class XmlBuilderTest extends UnitTestCase {
-    
+
     public $builder;
-    
+
     public function setUp() {
         $this->builder = new XmlBuilder;
         $this->builder->element()->formatOutput = false;
     }
-    
+
+    public function testSimpleDocument() {
+        $xml = $this->builder;
+        $xml->foo = 'bar';
+        $this->expectXml("<foo>bar</foo>");
+    }
+
+    public function testOneChild() {
+        $xml = $this->builder;
+        $xml->foo->bar = 'baz';
+        $this->expectXml("<foo><bar>baz</bar></foo>");
+    }
+
+    public function testMultipleChildren() {
+        $xml = $this->builder;
+        $xml->foo->bar = 'baz';
+        $xml->foo->john = 'doe';
+        $this->expectXml("<foo><bar>baz</bar><john>doe</john></foo>");
+    }
+
+    public function testSingleAttribute() {
+        $xml = $this->builder;
+        $xml->foo['bar']= 'baz';
+        $this->expectXml('<foo bar="baz"/>');
+    }
+
+    public function testMultipleAttributes() {
+        $xml = $this->builder;
+        $xml->foo['bar']= 'baz';
+        $xml->foo['john']= 'doe';
+        $this->expectXml('<foo bar="baz" john="doe"/>');
+    }
+
+    public function testAttributeInChild() {
+        $xml = $this->builder;
+        $xml->foo->bar['john']= 'doe';
+        $this->expectXml('<foo><bar john="doe"/></foo>');
+    }
+
+    public function testAttributeAndChildWithSameName() {
+        $xml = $this->builder;
+        $xml->foo['bar'] = 'baz';
+        $xml->foo->bar = 'baz';
+        $this->expectXml('<foo bar="baz"><bar>baz</bar></foo>');
+    }
+
     public function testUnset() {
         $xml = $this->builder;
         $xml->foo->bar = 'baz';
