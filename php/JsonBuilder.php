bab45091516c7ac4b8c1961dc6d77fa1fe3a0e3a
glaszig <glaszig@gmail.com>
2012-04-24 15:17:09 +0200

    json builder: adding multi-dimension handling, array access, tests

--- php/JsonBuilder.php
+++ php/JsonBuilder.php
@@ -1,18 +1,13 @@
 <?php
 
-class JsonBuilder {
+class JsonBuilder implements ArrayAccess {
 
-    protected $parent;
-    protected $struct;
+    protected $struct = array();
 
     public function __construct() {
         if (func_num_args() > 0) {
             $arg = func_get_arg(0);
-            if ($arg instanceof self) {
-                $this->parent = $arg;
-            } else {
-                $this->struct = $arg;
-            }
+            $this->struct = $arg;
         }
     }
 
@@ -20,8 +15,60 @@ class JsonBuilder {
         $this->struct[$key] = $value;
     }
 
+    public function __get($key) {
+        return $this->child($key);
+    }
+
+    public function __unset($key) {
+        if (isset($this->struct[$key])) {
+            unset($this->struct[$key]);
+        }
+    }
+
+    public function offsetExists($offset) {
+        return isset($this->struct[$offset]);
+    }
+
+    public function offsetGet($offset) {
+        return $this->struct[$offset];
+    }
+
+    public function offsetSet($offset, $value) {
+        $this->struct[$offset] = $value;
+    }
+
+    public function offsetUnset($offset) {
+        unset($this->struct[$offset]);
+    }
+
+    protected function child($name) {
+        if (!isset($this->struct[$name])) {
+            $this->struct[$name] = new self();
+        }
+        return $this->struct[$name];
+    }
+
+    public function toJson() {
+        $struct = null;
+        $this->build($struct);
+        return json_encode($struct);
+    }
+
+    public function build(&$struct) {
+        if (!is_array($this->struct)) {
+            $struct = $this->struct;
+            return;
+        }
+        foreach ($this->struct as $key => $value) {
+            if ($value instanceof self)
+                $value->build($struct[$key]);
+            else
+                $struct[$key] = $value;
+        }
+    }
+
     public function __toString() {
-        return json_encode($this->struct);
+        return $this->toJson();
     }
 
 }
--- php/JsonBuilderTest.php
+++ php/JsonBuilderTest.php
@@ -15,10 +15,14 @@ class JsonBuilderTest extends UnitTestCase {
         $null = new JsonBuilder(null);
         $int = new JsonBuilder(123);
         $str = new JsonBuilder('foo');
+        $obj = new JsonBuilder(array('key' => 'value'));
+        $ary = new JsonBuilder(array('str', null, 123));
 
         $this->assertEqual("$null", 'null');
         $this->assertEqual("$int", '123');
         $this->assertEqual("$str", '"foo"');
+        $this->assertEqual("$obj", '{"key":"value"}');
+        $this->assertEqual("$ary", '["str",null,123]');
     }
 
     public function testOneDimension() {
@@ -27,6 +31,56 @@ class JsonBuilderTest extends UnitTestCase {
 
         $expect = array('foo' => 'bar');
         $this->assertEqual("$json", json_encode($expect));
+    }
+
+    public function testMultiDimension() {
+        $json = $this->builder;
+        $json->foo->bar = "baz";
+
+        $expect = array('foo' => array(
+            'bar' => 'baz'
+        ));
+        $expect = json_encode($expect);
+        $this->assertEqual("$json", $expect);
+
+        $json->foo->int = 123;
+        $json->null = null;
+        $json->ary = array('key' => 'val');
+
+        $expect = array(
+            'foo' => array(
+                'bar' => 'baz',
+                'int' => 123
+            ),
+            'null' => null,
+            'ary' => array('key' => 'val')
+        );
+        $expect = json_encode($expect);
+        $this->assertEqual("$json", $expect);
+    }
+
+    public function testArrayAccess() {
+        $json = $this->builder;
+        $json->foo = array(123, "str");
+
+        $this->assertEqual($json->foo[0], 123);
+        $this->assertEqual($json->foo[1], "str");
+    }
+
+    public function testUnset() {
+        $json = $this->builder;
+        $json->foo->bar = "baz";
+
+        $expect = array('foo' => array(
+            'bar' => 'baz'
+        ));
+        $expect = json_encode($expect);
+        $this->assertEqual("$json", $expect);
+
+        unset($json->foo->bar);
+        $expect = array('foo' => null);
+        $expect = json_encode($expect);
+        $this->assertEqual("$json", $expect);
     }
 
 }
